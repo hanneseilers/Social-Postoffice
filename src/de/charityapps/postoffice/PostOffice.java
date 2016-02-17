f6ff8adb3d4868a297bbfd692eb1d37746d5a711
hanneseilers <mail@hanneseilers.de>
2016-02-17 21:31:54 +0100

    Addedempty main class

---
+++ src/de/charityapps/postoffice/PostOffice.java
@@ -0,0 +1,21 @@
+package de.charityapps.postoffice;
+
+/**
+ * Main class
+ * @author eilers
+ *
+ */
+public class PostOffice {
+	
+	/**
+	 * Constructor
+	 */
+	public PostOffice() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		new PostOffice();
+	}
+
+}
